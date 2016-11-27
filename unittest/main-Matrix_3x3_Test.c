62ba1397c7842ae45c8cbccb6b073a91be646fe7
Lyle Langpap <lyle.langpap@entiuum.com>
2016-11-27 10:13:25 -0400

    Test for Cramer's Rule

--- unittest/main-Matrix_3x3_Test.c
+++ unittest/main-Matrix_3x3_Test.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "Matrix_3x3.h"
 #include "Test.h"
 
@@ -29,6 +31,54 @@ int main(int argc, char **argv)
 		103.0f,
 		0.1f
 		);
+
+	// Cramer's Rule
+	Matrix_3x3_Init(&matrix, 
+		 2, -1,  6, 
+		-3,  4, -5, 
+		 8, -7, -9);
+
+	struct Matrix_3x1 matrix2;
+	Matrix_3x1_Init(&matrix2, 10, 11, 12);
+
+	struct Matrix_3x1 solution = Matrix_3x3_Cramers_Rule(&matrix, &matrix2);
+
+	Test_Range_Float("Cramer's Rule X", Matrix_3x1_Element(&solution, 1, 1), 1499.0f / 141.0f, 0.1f);
+	Test_Range_Float("Cramer's Rule Y", Matrix_3x1_Element(&solution, 2, 1), 1492.0f / 141.0f, 0.1f);
+	Test_Range_Float("Cramer's Rule Z", Matrix_3x1_Element(&solution, 3, 1), -16.0f / 141.0f, 0.01f);
+
+	// Cramer's Rule
+	Matrix_3x3_Init(&matrix, 
+		 2, -1,  6, 
+		-3,  4, -5, 
+		 8, -7, -9);
+
+	float denominator = Matrix_3x3_Determinant(&matrix);
+
+	Matrix_3x3_Init(&matrix, 
+		10, -1,  6, 
+		11,  4, -5, 
+		12, -7, -9);
+
+	float x = Matrix_3x3_Determinant(&matrix) / denominator;
+
+	Matrix_3x3_Init(&matrix, 
+		 2, 10,  6, 
+		-3, 11, -5, 
+		 8, 12, -9);
+
+	float y = Matrix_3x3_Determinant(&matrix) / denominator;
+
+	Matrix_3x3_Init(&matrix, 
+		 2, -1, 10, 
+		-3,  4, 11, 
+		 8, -7, 12);
+
+	float z = Matrix_3x3_Determinant(&matrix) / denominator;
+
+	Test_Range_Float("Cramer's Rule X - the long way", x, 1499.0f / 141.0f, 0.1f);
+	Test_Range_Float("Cramer's Rule Y - the long way", y, 1492.0f / 141.0f, 0.1f);
+	Test_Range_Float("Cramer's Rule Z - the long way", z, -16.0f / 141.0f, 0.01f);
 
 	return 0;
 }
